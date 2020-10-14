db1f0a7b32bf707f612331217ca957b04066e1a8
chrstphrvllrn <christopher.villarin.wo@gmail.com>
2020-10-14 20:49:05 +0800

    adding chat component

---
+++ client/src/components/Chat.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+class Chat extends React.Component {
+	render() {
+		return (
+		<div className="Chat">
+			<p>Hello World</p>
+		</div>
+ 		 );
+	}
+}
+
+export default Chat
